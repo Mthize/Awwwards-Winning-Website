0b15a881ed8f3d2643b6163e1c6fdea3284e61dc
Thapelo Mthize <122882874+Mthize@users.noreply.github.com>
2025-08-22 13:19:02 +0200

    feat(hero): add GSAP SplitText and scroll-triggered animations to HeroSection

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from './components/NavBar' 
+import HeroSection from './sections/HeroSection'
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all"; 
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <HeroSection />
+    </main>
+  )
+}
+
+export default App
+
---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,100 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import SplitText from "gsap/SplitText"; 
+
+const HeroSection = () => {
+
+
+
+useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
+
+    const tl = gsap.timeline({
+      delay: 1,
+    });
+
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+    )
+    .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.out",      
+        },  
+        "-=0.5"
+      );
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-content",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,   
+      },
+    });
+    
+    heroTl.to(".hero-content", {
+      rotation: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+  });
+
+
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        <img
+          src="images/static-img.png"
+          alt="hero-img"
+          className="absolute bottom-0 
+          left-1/2 -translate-x-1/2 object-auto 
+          scale-100 md:scale-150"
+        />
+
+        <div className="hero-content opacity-0">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Absolutely Delicious</h1>
+          </div>
+          <div
+            style={{
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100% )",
+            }}
+            className="hero-text-scroll"
+          >
+            <div>
+              <h1 className="hero-subtitle">Protein + Caffine</h1>
+            </div>
+          </div>
+
+          <h2>
+            Live boldly with SPYLT: turn ordinary moments into playful
+            adventures with every creamy, smooth sip.
+          </h2>
+
+          <div className="hero-button">
+            <p>Chug a SPYLT</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
