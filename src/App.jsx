2fb26c52a80725014ef7f4f9f7ccd467f6614b74
Thapelo Mthize <122882874+Mthize@users.noreply.github.com>
2025-08-27 08:55:52 +0200

    feat(flavor-section): add horizontal scroll animation with GSAP + text parallax effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import NavBar from './components/NavBar' 
 import HeroSection from './sections/HeroSection'
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all"; 
+import { ScrollSmoother, ScrollTrigger } from "gsap/all"; 
 import MessageSection from './sections/MessageSection'
+import FlavorSection from './sections/FlavorSection'
+import { useGSAP } from "@gsap/react";
 
-
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
   return (
     <main>
       <NavBar />
-      <HeroSection />
-      <MessageSection />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />          
+          <div></div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
